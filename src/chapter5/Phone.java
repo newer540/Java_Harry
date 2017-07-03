e5e31519f99127b0843dddc6085a33334935073f
hbagratyan <harry.bagratyan@yandex.ru>
2017-07-03 22:26:44 +0300

    1.07

---
+++ src/chapter5/Phone.java
@@ -0,0 +1,32 @@
+package chapter5;
+/**
+ * Created by harry on 03.07.2017.
+ */
+class Phone {
+    public static void main(String args[]) {
+        String numbers[][] = {
+                {"Tom", "555-3322"},
+                {"Mary", "555-8976"},
+                {"Jon", "555-1037"},
+                {"Rachel", "555-1400"},
+        };
+        int i;
+
+        // Для того чтобы воспользоваться программой,
+        // ей нужно передать один аргумент командной строки
+        if (args.length != 1)
+            System.out.println("Иcпoльзoвaниe: java Phone <имя>");
+        else {
+            for (i = 0; i < numbers.length; i++) {
+                if (numbers[i][0].equals(args[0])) {
+                    System.out.println(numbers[i][0] + ": " +
+                            numbers[i][1]);
+                    break;
+                }
+            }
+            if (i == numbers.length)
+                System.out.println("Имя не найдено.");
+        }
+    }
+}
+
