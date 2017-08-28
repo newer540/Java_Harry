07560b36e61bab48b04ec7a09befbe137270d9a7
hbagratyan <harry.bagratyan@yandex.ru>
2017-08-28 19:54:58 +0300

    1.07

--- src/chapter3/SqrRoot.java
+++ src/chapter3/SqrRoot.java
@@ -7,13 +7,15 @@ public class SqrRoot {
     public static void main(String args[]) {
         double num, sroot, rerr;
 
-        for(num = 1.0; num < 100.0; num++) {
+        for(num = 1.0; num < 36.0; num++) {
             sroot = Math.sqrt(num);
-            System.out.println("Square root from " + num + "equals"  + sroot);
+            System.out.println("Square root from " + num + " equals "  + sroot);
             //вычислить ошибку округления
             rerr = num - (sroot * sroot);
-            System.out.println("R. error: " + rerr);
-            System.out.println();
+            if (rerr != 0) {
+                System.out.println("R. error: " + rerr);
+                System.out.println();
+            }
         }
     }
 }
---
+++ src/chapter6/Quene.java
@@ -0,0 +1,30 @@
+package chapter6;
+
+// An improved queue class for characters.
+class Queue {
+    // these members are now private
+    private char q[]; // this array holds the queue
+    private int putloc, getloc; // the put and get indices
+    Queue(int size) {
+        q = new char[size+1]; // allocate memory for queue
+        putloc = getloc = 0;
+    }
+    // Put a character into the queue.
+    void put(char ch) {
+        if(putloc==q.length-1) {
+            System.out.println(" – Queue is full.");
+            return;
+        }
+        putloc++;
+        q[putloc] = ch;
+    }
+// Get a character from the queue.
+char get() {
+    if(getloc == putloc) {
+        System.out.println(" – Queue is empty.");
+        return (char) 0;
+    }
+    getloc++;
+    return q[getloc];
+}
+}
