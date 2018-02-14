eec4d3b587f3d58638933ebc4e5fddbec394e1de
hbagratyan <harry.bagratyan@yandex.com>
2018-02-14 13:15:58 +0300

    ch11

--- src/codewars/MaskifyCodewars.java
+++ src/codewars/MaskifyCodewars.java
@@ -1,7 +1,5 @@
-/* package codewars;
-import java.io.Console;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+package codewars;
+
 
 /*Usually when you buy something, you're asked whether your credit card number,
 phone number or answer to your most secret question is still correct. However,
@@ -10,20 +8,25 @@ screen. Instead, we mask it.
 
 Your task is to write a function maskify, which changes all but the last four
 characters into '#'. */
-/*class Maskify {
+class Maskify {
     public static String maskify(String str) {
-        Console console = System.console();
         String finalString; // Result string
         String subString; // Opened part of string
         String maskedSub; // Closed part of string
         String PrivSub;
 
 
+
         if (str.length() > 4) {
             subString = str.substring(str.length() - 4, str.length());
             maskedSub = str.substring(0, str.length() - 4);
-            PrivSub = maskedSub.replaceAll("")
+            char [] PrivCharArray = new char[maskedSub.length()];
+            for (int i = 0; i < maskedSub.length(); i++){
+                PrivCharArray[i]= '#';
+            }
 
+            PrivSub = new String(PrivCharArray);
+            finalString = PrivSub.concat(subString);
 
             return finalString;
         }
@@ -36,4 +39,3 @@ public class MaskifyCodewars {
         System.out.println(Maskify.maskify("abcd654654654654efg"));
     }
 }
-*/
